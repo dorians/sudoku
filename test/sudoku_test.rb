4583d573cde573fb0c28a00083c51cc4353c5470
Dorian Sarnowski <dorian.sarnowski@gmail.com>
2011-09-04 18:17:48 +0200

    ruby 1.9.2 fix in tests

--- test/sudoku_test.rb
+++ test/sudoku_test.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'test/unit'
 require 'sudoku'
+require 'sudoku_solver'
 require 'pp'
 
 class SudokuTest < Test::Unit::TestCase
